9aac06ac1d609ff3b514eb5979f15488aad000cd
GuidoRial <rialguido@gmail.com>
2022-03-23 16:36:38 -0300

    changed folder and clean code

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,12 @@
 import "./App.css";
-import { useEffect, useState } from "react";
-
 import Header from "./components/Header/Header";
 import SecretsContainer from "./components/SecretsContainer/SecretsContainer";
 
 function App() {
-
-
-
-
-
-
-
     return (
         <div className="App">
             <Header />
-            
             <SecretsContainer />
-            {/*<div className="secrets-display">
-                {secrets.map((secret) => (
-                    <div className="secret">
-                        {secret.edad}
-                        {secret.sexo}
-                        {secret.secreto}
-                    </div>
-                ))}
-            </div>
-            <form>
-                <input
-                    type="number"
-                    placeholder="Edad..."
-                    onChange={(e) => setEdad(e.target.value)}
-                />
-                <input
-                    type="text"
-                    placeholder="Sexo..."
-                    onChange={(e) => setSexo(e.target.value)}
-                />
-                <input
-                    type="text"
-                    placeholder="Secreto..."
-                    onChange={(e) => setSecreto(e.target.value)}
-                />
-                <button onClick={sendSecret}>ENVIAR</button>
-            </form> */}
         </div>
     );
 }
